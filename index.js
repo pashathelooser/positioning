2e1aea5814c0d380ccd60cd8aedc621ab8ded29f
pashathelooser <pavelvatutin08@gmail.com>
2026-04-07 18:43:26 +0500

    Task4_done

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const openBtn = document.querySelector('#openModal');
 const closeBtn = document.querySelector('#closeModal');
 const overlay = document.querySelector('#modalOverlay');
+const progressFill = document.querySelector("#progressFill")
 
 openBtn.addEventListener('click', function() {
     overlay.classList.add('active');
+
+    progressFill.style.width = '0%';
+
+    setTimeout(() => {
+        progressFill.style.width = '90%';
+    }, 100);
 });
 
 closeBtn.addEventListener('click', function() {
     overlay.classList.remove('active');
 });
+
